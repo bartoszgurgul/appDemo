e7186d29402b5f6f72984bb122252fd1b2097c71
bgurgul <bgurgul@dell-bgur.2KMM.local>
2019-04-15 16:04:38 +0200

    zmiany

--- appDemo/src/main/java/appdemo/utilities/UserUtilities.java
+++ appDemo/src/main/java/appdemo/utilities/UserUtilities.java
@@ -1,8 +1,22 @@
 package appdemo.utilities;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import appdemo.entity.User;
 
 public class UserUtilities {
 		
@@ -21,6 +35,53 @@ public class UserUtilities {
 		}
 		
 		return userName;
+	}
+	
+	public static List<User> userDataLoader(File file){
+		
+		List<User> userList = new ArrayList<>();
+		
+		try {
+			DocumentBuilderFactory dbFactor = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactor.newDocumentBuilder();
+			Document doc = dBuilder.parse(file);
+			
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("user");
+			for(int i = 0;i < nList.getLength();i++) {
+				Node n = nList.item(i);
+				if(n.getNodeType() == Node.ELEMENT_NODE) {
+					Element e = (Element) n;
+					
+					User u = new User();
+					u.setName(e.getElementsByTagName("name").item(0).getTextContent());
+					u.setEmail(e.getElementsByTagName("email").item(0).getTextContent());
+					u.setActive(
+						Integer.valueOf(
+								e.getElementsByTagName("active")
+									.item(0)
+									.getTextContent()));	
+					u.setLastName(e.getElementsByTagName("lastname")
+							.item(0).getTextContent());
+					u.setNrRoli(Integer.valueOf(e.getElementsByTagName("nrroli")
+							.item(0)
+							.getTextContent()));
+					u.setPassword(e.getElementsByTagName("password")
+							.item(0)
+							.getTextContent());
+					userList.add(u);
+					
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+			
+		
+		
+		
+		return userList;
 	}
 
 }
